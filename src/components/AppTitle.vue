c5e9f598abd267d9a086f3ca62a39c7b6b8f800e
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-04-30 14:56:30 +0200

    feat: scaffolding + title working

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>  
+    export default {
+        data () {
+            return {
+
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <h1>La mia prima App con Vite!</h1>
+
+</template>
+
+<style>
+
+</style>
